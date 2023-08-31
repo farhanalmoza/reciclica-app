93fb768104690870f74f5c7b9ba052329c57de8f
farkhan_tbrg <farkhan2@thebigrichgroup.com>
2023-08-31 17:37:14 +0700

    login form validations

--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -3,6 +3,7 @@ import { LoginPage } from './login.page';
 import { IonicModule } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AppRoutingModule } from 'src/app/app-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginPage', () => {
   let component: LoginPage;
@@ -14,7 +15,8 @@ describe('LoginPage', () => {
       declarations: [ LoginPage ],
       imports: [
         IonicModule.forRoot(),
-        AppRoutingModule
+        AppRoutingModule,
+        ReactiveFormsModule
       ]
     }).compileComponents();
 
@@ -22,6 +24,12 @@ describe('LoginPage', () => {
     router = TestBed.get(Router);
     component = fixture.componentInstance;
   }));
+
+  it('should create form on init', () => {
+    component.ngOnInit();
+
+    expect(component.form).not.toBeUndefined();
+  });
 
   it('should go to home page on login', () => {
     spyOn(router, 'navigate');
