b0ea61f38a8e7190d95b035e0ae2baba65111652
farkhan_tbrg <farkhan2@thebigrichgroup.com>
2023-08-31 15:46:30 +0700

    see & create pickup call(s)

--- src/app/pages/loader/loader.page.spec.ts
+++ src/app/pages/loader/loader.page.spec.ts
@@ -1,17 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
 import { LoaderPage } from './loader.page';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoaderPage', () => {
   let component: LoaderPage;
   let fixture: ComponentFixture<LoaderPage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoaderPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoaderPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should go to login page after load', fakeAsync(() => {
+    spyOn(router, 'navigate');
+    component.ngOnInit();
+    tick(1500);
+    expect(router.navigate).toHaveBeenCalledWith(['login'])
+  }));
 });
--- src/app/pages/login/login.page.spec.ts
+++ src/app/pages/login/login.page.spec.ts
@@ -1,17 +1,37 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { IonicModule } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AppRoutingModule } from 'src/app/app-routing.module';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginPage ],
+      imports: [
+        IonicModule.forRoot(),
+        AppRoutingModule
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should go to home page on login', () => {
+    spyOn(router, 'navigate');
+    component.login();
+    expect(router.navigate).toHaveBeenCalledWith(['home']);
+  });
+
+  it('should go to register page on register', () => {
+    spyOn(router, 'navigate');
+    component.register();
+    expect(router.navigate).toHaveBeenCalledWith(['register']);
   });
 });
